cfd33bc8d7d9106f5aedf28f1fe4fea0ba05d04e
Sven <nebel.sven@gmail.com>
2017-06-03 09:22:15 +0100

    bump version for the first release

--- word_ladder/word_ladder.py
+++ word_ladder/word_ladder.py
@@ -2,7 +2,7 @@ from collections import defaultdict, deque
 from itertools import product
 from word_ladder.errors import WordsNotDefined
 
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 
 
 class Graph(object):
