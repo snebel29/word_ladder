df2b78599ae118c529d7bb0877b32f229fb1846c
Sven <nebel.sven@gmail.com>
2017-06-06 12:39:01 +0100

    Packaging for Pypi

--- setup.py
+++ setup.py
@@ -5,23 +5,37 @@ from word_ladder import __version__
 with open('README.rst') as f:
     long_description = f.read()
 
+with open('docs/docs_requirements.txt') as f:
+    doc_requirements = f.read().splitlines()
+
 requirements = ['docopt']
-dev_requirements = ['nose', 'sphinx', 'sphinxcontrib-napoleon', 'sphinx_rtd_theme']
+dev_requirements = ['nose'] + doc_requirements
 
 setup(
     name='word_ladder',
+    packages=['word_ladder'],
     version=__version__,
-    description='Find path between two words, changing one letter each step',
+    description='word ladder find path between two words, changing one letter each step',
     long_description=long_description,
     url='https://github.com/snebel29/word_ladder',
     author='Sven Nebel',
     author_email='nebel.sven@gmail.com',
     license='GPL',
-    packages=['word_ladder'],
+    keywords=['word', 'ladder'],
+    download_url='https://github.com/snebel29/word_ladder/archive/{0}.tar.gz'.format(__version__),
     install_requires=requirements,
     extras_require={'dev': dev_requirements},
     zip_safe=False,
     entry_points={
         'console_scripts': ['word_ladder=word_ladder.cli:run']
-    }
+    },
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: POSIX',
+    ]
 )
